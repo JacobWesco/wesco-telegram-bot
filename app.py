e6f20428c25fe80614e87c8574ee0240a903629a
JacobWesco <jacob@wescotrans.pl>
2025-07-25 14:05:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,6 @@ def webhook():
 
 # --- uruchomienie serwera Flask ---
 if __name__ == "__main__":
+     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
